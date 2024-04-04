b02732970283c67f019780aa6272276d7f73f818
bunchi3 <s.franceschina@campus.unimib.it>
2024-04-04 16:40:25 +0200

    Aggiunto dati2

---
+++ lablib.py
@@ -0,0 +1,7 @@
+from iminuit import Minuit
+from iminuit.cost import LeastSquares, ExtendedBinnedNLL
+
+def LS_fit():
+    return 0
+# https://www.geeksforgeeks.org/args-kwargs-python/?ref=lbp
+# kwargs è esattamente ciò che serve
